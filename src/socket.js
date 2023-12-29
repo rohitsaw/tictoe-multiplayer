5a7621f43db9850bdc53c315cce97e2939504792
Rohit <rsaw409@gmail.com>
2023-12-29 10:29:39 +0530

    update server address

--- src/socket.js
+++ src/socket.js
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 // "undefined" means the URL will be computed from the `window.location` object
 const URL =
   process.env.NODE_ENV === "production"
-    ? "https://rsaw409-tictoe-endpoint.onrender.com"
+    ? "https://rsaw409-portfolio-backend.onrender.com"
     : "http://localhost:3001";
 
 export const socket = io(URL);
